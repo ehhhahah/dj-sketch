ecb6a98bb40dc07e9382b77cf25e273d3111c772
Wojtek <ehhhahah@gmail.com>
2024-12-25 20:19:10 +0100

    split css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,12 @@ import { AudioUploadSchema } from '../../constants/serverSchemas'
 import AudioRecorder from './pages/Microphone'
 import DisplayNodes from './pages/DisplayNodes'
 
+import './stylees/divider.css'
+
 function App() {
   const [uploads, setUploads] = useState<AudioUploadSchema[]>([])
   const [dividerY, setDividerY] = useState(window.innerHeight / 2)
-
+  const [dividerX, setDividerX] = useState(window.innerWidth / 2)
   const dividerRef = useRef<HTMLDivElement>(null)
 
   const getList = useCallback(async () => {
@@ -25,11 +27,18 @@ function App() {
 
   const handleMouseDown = (e: React.MouseEvent) => {
     const startY = e.clientY
+    const startX = e.clientX
     const startHeight = dividerY
+    const startWidth = dividerX
 
     const handleMouseMove = (e: MouseEvent) => {
-      const newHeight = startHeight + (e.clientY - startY)
-      setDividerY(newHeight)
+      if (window.innerWidth >= 768) {
+        const newWidth = startWidth + (e.clientX - startX)
+        setDividerX(newWidth)
+      } else {
+        const newHeight = startHeight + (e.clientY - startY)
+        setDividerY(newHeight)
+      }
     }
 
     const handleMouseUp = () => {
@@ -42,25 +51,37 @@ function App() {
   }
 
   const handleDoubleClick = () => {
-    setDividerY(window.innerHeight / 2)
+    if (window.innerWidth >= 768) {
+      setDividerX(window.innerWidth / 2)
+    } else {
+      setDividerY(window.innerHeight / 2)
+    }
   }
 
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col md:flex-row h-screen'>
       <Toaster position='top-right' />
-      <div className='overflow-y-auto' style={{ height: dividerY }}>
+      <div className='overflow-y-auto md:overflow-x-auto' style={{ height: dividerY, width: dividerX }}>
         <AudioRecorder getList={getList} />
         <AudioUploader getList={getList} />
         <AudioGenerator getList={getList} />
       </div>
-      <div ref={dividerRef} className='break-line' onMouseDown={handleMouseDown} onDoubleClick={handleDoubleClick}>
+      <div
+        ref={dividerRef}
+        className='break-line'
+        onMouseDown={handleMouseDown}
+        onDoubleClick={handleDoubleClick}
+        style={{ cursor: window.innerWidth >= 768 ? 'col-resize' : 'row-resize' }}>
         <div className='resize-dot' />
       </div>
-      <div className='overflow-y-auto' style={{ height: `calc(100% - ${dividerY}px - 2px)` }}>
+      <div
+        className='overflow-y-auto md:overflow-x-auto'
+        style={{ height: `calc(100% - ${dividerY}px - 2px)`, width: `calc(100% - ${dividerX}px - 2px)` }}>
         <ListUploads uploads={uploads} getList={getList} />
         <DisplayNodes uploads={uploads} />
       </div>
     </div>
   )
 }
+
 export default App
